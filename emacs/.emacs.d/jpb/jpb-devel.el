a374a8ab540bef8d72b504bcb06a579e5802cb39
kostspielig <kostspielig@gmail.com>
2020-04-02 10:19:40 +0200

    Comment out lines that break stuff

--- emacs/.emacs.d/jpb/jpb-devel.el
+++ emacs/.emacs.d/jpb/jpb-devel.el
@@ -57,18 +57,18 @@
 ;;
 ;; Fix C++ lambdas
 ;;
-(defadvice c-lineup-arglist (around my activate)
-  "Improve indentation of continued C++11 lambda function opened as argument."
-  (setq ad-return-value
-        (if (and (equal major-mode 'c++-mode)
-                 (ignore-errors
-                   (save-excursion
-                     (goto-char (c-langelem-pos langelem))
-                     ;; Detect "[...](" or "[...]{". preceded by "," or "(",
-                     ;;   and with unclosed brace.
-                     (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
-            0                           ; no additional indent
-          ad-do-it)))                   ; default behavior
+;; (defadvice c-lineup-arglist (around my activate)
+;;   "Improve indentation of continued C++11 lambda function opened as argument."
+;;   (setq ad-return-value
+;;         (if (and (equal major-mode 'c++-mode)
+;;                  (ignore-errors
+;;                    (save-excursion
+;;                      (goto-char (c-langelem-pos langelem))
+;;                      ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+;;                      ;;   and with unclosed brace.
+;;                      (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+;;             0                           ; no additional indent
+;;           ad-do-it)))                   ; default behavior
 
 
 ;; flycheck
