c93e21783099ba7e0ce670ce2abbbaa7cf27f5db
kostspielig <kostspielig@gmail.com>
2023-03-02 09:47:20 +0100

    Comment out marmalade as it is not responding

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,8 +19,8 @@
 (fset 'xterm-color-unfontify-region 'font-lock-default-unfontify-region)
 
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;;(add-to-list 'package-archives
+;;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
