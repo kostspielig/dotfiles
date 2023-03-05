218e7cdc2755ee1359c893ff205d7916dde3d2b4
kostspielig <kostspielig@gmail.com>
2023-03-06 00:08:33 +0100

    Create `cdenv` command.

--- bash/.bash.d/functions.bash
+++ bash/.bash.d/functions.bash
@@ -27,3 +27,19 @@ mkcd()
 {
     test -d "$1" || mkdir "$1" && cd "$1"
 }
+
+# Public: Change to a development directory and load .env file.
+#
+# Takes a single argument that can either be full path of the project or name of
+# a project under ~/dev/* directory
+#
+# $1 - [Path to] project.
+#
+# Examples
+#
+#   cdenv ~/dev/maria
+#
+cdenv()
+{
+    if test -d ~/dev/"$1"; then cd ~/dev/"$1"; else echo cd "$1"; fi && source .env
+}
